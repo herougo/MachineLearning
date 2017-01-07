2ced378d5f8cb6c7d49d46fb6f7d38f61802d56f
Henri Romel <henri@localhost.localdomain>
2017-01-07 16:54:30 -0500

    update

--- python/helpers/PlottingWrapper.py
+++ python/helpers/PlottingWrapper.py
@@ -1,17 +1,39 @@
-import matplotlib.pyplot as plt
-import pandas as Series
+# alternate interface for matplotlib to display plots with one
+# line of python
 
+# To do: account for multiple lines in the same graph
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Purpose: if a panda series is passed, format it to be compatible
+#          with matplotlib
 def formatArray(x):
 	if type(x) == pd.Series:
 		x = x.values
 	return x
 
 # histogram
-def hist():
+def histogram():
     return ""
 
-def scatterplot(x_values, y_values, x_label="", y_label="", title=""):
-    plt.plot(x_values, y_values, 'ro')
+# box and whisker plot
+def boxplot():
+	return ""
+
+# confusion matrix as image
+def confusionmatrix():
+	return ""
+
+# plot signals given matrix
+def signals():
+	return ""
+
+# scatter plot
+def scatter(x_values, y_values, x_label="", y_label="", title=""):
+    x_values = formatArray(x_values)
+    y_values = formatArray(y_values)
+    plt.plot(x_values, y_values, 'o')
     if title != "":
         plt.title(title)
     if x_label != "":
@@ -21,8 +43,10 @@ def scatterplot(x_values, y_values, x_label="", y_label="", title=""):
     plt.show()
     
 
-# line graph
-def linegraph(x_values, y_values, x_label="", y_label="", title=""):
+# line plot
+def line(x_values, y_values, x_label="", y_label="", title=""):
+    x_values = formatArray(x_values)
+    y_values = formatArray(y_values)
     plt.plot(x_values, y_values)
     if title != "":
         plt.title(title)
